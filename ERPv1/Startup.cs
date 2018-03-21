6478aa5f12a20a1de6c503aae3c5e73dc38af9dc
Timo Dave Sattler <tds@netzplan.de>
2018-03-21 09:45:52 +0100

    Add project files.

---
+++ ERPv1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ERPv1.Startup))]
+namespace ERPv1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
