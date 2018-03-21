1acfaa5833642d8868934b4e6cd30ff0a4198683
Timo Dave Sattler <tds@netzplan.de>
2018-03-21 10:29:01 +0100

    Standard Controllers added

---
+++ ERPv1/Controllers/AuftragsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ERPv1.Models;
+
+namespace ERPv1.Controllers
+{
+    public class AuftragsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Auftrags
+        public ActionResult Index()
+        {
+            return View(db.Auftrag.ToList());
+        }
+
+        // GET: Auftrags/Details/5
+        public ActionResult Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Auftrag auftrag = db.Auftrag.Find(id);
+            if (auftrag == null)
+            {
+                return HttpNotFound();
+            }
+            return View(auftrag);
+        }
+
+        // GET: Auftrags/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Auftrags/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID")] Auftrag auftrag)
+        {
+            if (ModelState.IsValid)
+            {
+                auftrag.ID = Guid.NewGuid();
+                db.Auftrag.Add(auftrag);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(auftrag);
+        }
+
+        // GET: Auftrags/Edit/5
+        public ActionResult Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Auftrag auftrag = db.Auftrag.Find(id);
+            if (auftrag == null)
+            {
+                return HttpNotFound();
+            }
+            return View(auftrag);
+        }
+
+        // POST: Auftrags/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID")] Auftrag auftrag)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(auftrag).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(auftrag);
+        }
+
+        // GET: Auftrags/Delete/5
+        public ActionResult Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Auftrag auftrag = db.Auftrag.Find(id);
+            if (auftrag == null)
+            {
+                return HttpNotFound();
+            }
+            return View(auftrag);
+        }
+
+        // POST: Auftrags/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(Guid id)
+        {
+            Auftrag auftrag = db.Auftrag.Find(id);
+            db.Auftrag.Remove(auftrag);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ERPv1/Controllers/KundeController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ERPv1.Models;
+
+namespace ERPv1.Controllers
+{
+    public class KundeController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Kunde
+        public ActionResult Index()
+        {
+            return View(db.Kunden.ToList());
+        }
+
+        // GET: Kunde/Details/5
+        public ActionResult Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Kunde kunde = db.Kunden.Find(id);
+            if (kunde == null)
+            {
+                return HttpNotFound();
+            }
+            return View(kunde);
+        }
+
+        // GET: Kunde/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Kunde/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Vorname,Nachname,IsFirma,Email,PLZ,Ort,Addresse,Tel,KurzBezeichnung,Gelöscht")] Kunde kunde)
+        {
+            if (ModelState.IsValid)
+            {
+                kunde.ID = Guid.NewGuid();
+                db.Kunden.Add(kunde);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(kunde);
+        }
+
+        // GET: Kunde/Edit/5
+        public ActionResult Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Kunde kunde = db.Kunden.Find(id);
+            if (kunde == null)
+            {
+                return HttpNotFound();
+            }
+            return View(kunde);
+        }
+
+        // POST: Kunde/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Vorname,Nachname,IsFirma,Email,PLZ,Ort,Addresse,Tel,KurzBezeichnung,Gelöscht")] Kunde kunde)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(kunde).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(kunde);
+        }
+
+        // GET: Kunde/Delete/5
+        public ActionResult Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Kunde kunde = db.Kunden.Find(id);
+            if (kunde == null)
+            {
+                return HttpNotFound();
+            }
+            return View(kunde);
+        }
+
+        // POST: Kunde/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(Guid id)
+        {
+            Kunde kunde = db.Kunden.Find(id);
+            db.Kunden.Remove(kunde);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
