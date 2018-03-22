8df19b61c02bfb302366c6e04807b8612fd53980
KevinCopyrightException <35264012+KevinCopyrightException@users.noreply.github.com>
2018-03-22 12:36:25 +0100

    AppUser Anpassung #2

--- ERPv1/Migrations/Configuration.cs
+++ ERPv1/Migrations/Configuration.cs
@@ -1,28 +1,54 @@
+using ERPv1.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
 namespace ERPv1.Migrations
 {
     using System.Data.Entity.Migrations;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<ERPv1.Models.ApplicationDbContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<Models.ApplicationDbContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
         }
 
-        protected override void Seed(ERPv1.Models.ApplicationDbContext context)
+        protected override void Seed(Models.ApplicationDbContext context)
         {
-            //  This method will be called after migrating to the latest version.
+            
+            var userStore = new UserStore<ApplicationUser>(context);
+            var userManager = new UserManager<ApplicationUser>(userStore);
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>());
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            // Rollen erzeugen
+            if (!roleManager.RoleExists("Administration"))
+                roleManager.Create(new IdentityRole("Administration"));
+
+            if (!roleManager.RoleExists("Abteilungsleiter"))
+                roleManager.Create(new IdentityRole("Abteilungsleiter"));
+
+            if (!roleManager.RoleExists("Mitarbeiter"))
+                roleManager.Create(new IdentityRole("Mitarbeiter"));
+
+            // User erzeugen
+            var adminUser = new ApplicationUser
+            {
+                Email = "admin@admin.de",
+                UserName = "admin@admin.de",
+                EmailConfirmed = true,
+                FirstName = "Walter",
+                LastName = "Röhrich"
+            };
+
+            if (userManager.FindByEmail("admin@admin.de") == null)
+            {
+                userManager.Create(adminUser, "Admin123456#");
+                userManager.AddToRole(adminUser.Id, "Administration");
+            }
+
+            // Anderweitige Änderungen speichern
+            context.SaveChanges();
+
         }
     }
 }
