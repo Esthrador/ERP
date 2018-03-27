94410a7c26ad270f07d79cc3df6213c20ad648c7
KevinCopyrightException <35264012+KevinCopyrightException@users.noreply.github.com>
2018-03-27 13:49:13 +0200

    ´Waren Create Anpassung

--- ERPv1/Extensions/HtmlHelperExtensions.cs
+++ ERPv1/Extensions/HtmlHelperExtensions.cs
@@ -8,22 +8,6 @@ namespace ERPv1.Extensions
 {
     public static class HtmlHelperExtensions
     {
-        /// <summary>
-        /// Erweiterter AntiForgeryToken mit zusätzlichem Html Element.
-        /// </summary>
-        /// <param name="helper">HtmlHelper</param>
-        /// <param name="additionalHtml">Zusätzliches Html in Form eines Span mit Display None</param>
-        /// <returns>AntiForgeryToken</returns>
-        public static MvcHtmlString AntiForgeryToken(this HtmlHelper helper, bool additionalHtml)
-        {
-            var antiForgeryToken = helper.AntiForgeryToken();
 
-            var defaultString = antiForgeryToken.ToString();
-            var defaultStringLength = defaultString.Length;
-
-            var htmlString = MvcHtmlString.Create(defaultString.Insert(defaultStringLength, "</span>").Insert(0, "<span style=\"display:none\">"));
-
-            return MvcHtmlString.Empty;
-        }
     }
 }
