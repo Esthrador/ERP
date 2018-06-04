7c7e00b9ab735c6847e8258345b9c0d52727e099
KevinCopyrightException <kevin.g_ans@web.de>
2018-06-04 14:20:44 +0200

    + Rechnung

---
+++ ERPv1/Controllers/RechnungController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ERPv1.Models.ViewModels;
+using HiQPdf;
+using Microsoft.Ajax.Utilities;
+
+namespace ERPv1.Controllers
+{
+    [Authorize]
+    public class RechnungController : Controller
+    {
+        [AllowAnonymous]
+        public ActionResult ShowBillForContract()
+        {
+            var vm = new RechnungViewModel
+            {
+                Auftrag = null
+            };
+
+            return View("~/Views/Shared/_Rechnung.cshtml", vm);
+        }
+
+        public ActionResult GenerateBillForContract(Guid auftragId)
+        {
+            // Create the HTML to PDF converter
+            HtmlToPdf htmlToPdfConverter = new HtmlToPdf();
+
+            // Set PDF page margins
+            htmlToPdfConverter.Document.Margins = new PdfMargins(5);
+
+            // Convert URL to a PDF memory buffer
+            string url = Url.Action("ShowBillForContract", "Rechnung", null, Request.Url.Scheme);
+
+            var pdfBuffer = htmlToPdfConverter.ConvertUrlToMemory(url);
+
+            return File(pdfBuffer, "application/pdf");
+        }
+    }
+}
---
+++ ERPv1/Models/ViewModels/RechnungViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ERPv1.Models.ViewModels
+{
+    public class RechnungViewModel
+    {
+        public Auftrag Auftrag { get; set; }
+    }
+}
